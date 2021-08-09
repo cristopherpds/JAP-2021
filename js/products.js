e6e4d783a32a151317275074b1051f3b88825357
Cristopher Paiva <josuepaiva7@gmail.com>
2021-08-08 23:09:12 -0300

    The request was made to PRODUCTS_URL and the information was shown through products.js

--- js/products.js
+++ js/products.js
@@ -1,6 +1,46 @@
+let productsArray = [];
+
+const showCategoriesList = (array) => {
+
+	let htmlContentToAppend = "";
+	for (let i = 0; i < array.length; i++) {
+		let products = array[i];
+
+		htmlContentToAppend += `
+		<div class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ products.name + `</h4>
+                            <small class="text-muted">` + products.soldCount + ` artículos.</small>
+                        </div>
+						<div>
+                    <p>`+ products.description + `</p>
+                    <p> A tan solo `+ products.currency + "   " + products.cost + `</p>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            `
+
+		document.getElementById("product-list-container").innerHTML = htmlContentToAppend;
+	}
+}
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+	getJSONData(PRODUCTS_URL).then((resObj) => {
+		if (resObj.status === 'ok') {
+			productsArray = resObj.data;
+			console.log(productsArray);
+			showCategoriesList(productsArray);
+		}
+	})
 });
