d43e755133f2b8a0e1fc98a5be20cb1cdad54e04
Cristopher <josuepaiva7@gmail.com>
2021-09-06 23:03:00 -0300

    A web request was made to URL with an indetifier of the product where all the detailed information  about it can be found. All informations is displayed in HTML.

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,45 @@
+let product = {};
+
+
+const showImagesGallery = (array) => {
+  let htmlContentToAppend = '';
+  for (let i = 0; i < array.length; i++) {
+    let imageSrc = array[i];
+
+    htmlContentToAppend += `
+    <div class="col-lg-3 col-md-4 col-6">
+      <div class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="` + imageSrc + `" alt="">
+      </div>
+    </div>
+    `
+  }
+  document.getElementById('productsImagesWrapper').innerHTML = htmlContentToAppend;
+}
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
+  getJSONData(PRODUCT_INFO_URL).then((resObj) => {
+    if (resObj.status === 'ok') {
+      let product = resObj.data;
+      console.log(product);
 
+      let productNameHTML = document.getElementById('productName');
+      let productDescriptionHTML = document.getElementById('productDescription');
+      let productCostHTML = document.getElementById('productCost');
+      let productCurrencyHTML = document.getElementById('productCurrency');
+      let productSoldCountHTML = document.getElementById('productSoldCount');
+      let productCategoryHTML = document.getElementById('productCategory');
+
+      productNameHTML.innerHTML = product.name;
+      productDescriptionHTML.innerHTML = product.description;
+      productCostHTML.innerHTML = product.cost;
+      productCurrencyHTML.innerHTML = product.currency;
+      productSoldCountHTML.innerHTML = product.soldCount;
+      productCategoryHTML.innerHTML = product.category;
+
+      showImagesGallery(product.images);
+    }
+  });
 });
