010ac073f5db0c6eb964aebd47903b0d6bc514f9
Cristopher <josuepaiva7@gmail.com>
2021-09-20 19:34:57 -0300

    A web request was made to a URL where all the detailed information of a product is found. And it unfolded in HTML
    A web request was made to a URL where the preloaded comments and score of the product are found. And it is shown in HTML
    
    The necessary graphic controls were carried out to be able to make a new comment and punctuation. The sending of data will be fictitious, that is, the sending of the same will not be implemented

--- js/product-info.js
+++ js/product-info.js
@@ -16,6 +16,41 @@ const showImagesGallery = (array) => {
   document.getElementById('productsImagesWrapper').innerHTML = htmlContentToAppend;
 }
 
+//Funcion que "recorre las estrellas y las pinta" para poder agregarlas a un nuevo comentario
+function addStars(ths, sno) {
+  for (var i = 1; i <= 5; i++) {
+    var cur = document.getElementById('star' + i)
+    cur.className = "fa fa-star"
+  }
+
+  for (var i = 1; i <= sno; i++) {
+    var cur = document.getElementById('star' + i)
+    if (cur.className == "fa fa-star") {
+      cur.className = "fa fa-star checked"
+    }
+  }
+
+  rating = sno;
+  return rating;
+}
+
+//Funcion para mostrar estrellas en un comentario nuevo
+const showRating = (rating) => {
+  let htmlScore = '';
+  let stars = '';
+
+  for (let i = 1; i <= 5; i++) {
+    if (i <= rating) {
+      stars += `<i class="fa fa-star checked"></i>`;
+    } else {
+      stars += `<i class="fa fa-star"></i>`;
+    }
+  }
+  htmlScore = `<span> ${stars} </span>`
+  return htmlScore
+}
+
+
 const showStars = (productInfo) => {
   for (let i = 0; i < productInfo.length; i++) {
     const product = productInfo[i];
@@ -23,6 +58,7 @@ const showStars = (productInfo) => {
     document.getElementsByClassName("starsContainer")[i].innerHTML += `<span class="fa fa-star"></span>`.repeat(5 - product.score);
   }
 }
+
 
 const showReviews = (productInfo) => {
   let reviewsHtmlContentToAppend = [];
@@ -35,7 +71,7 @@ const showReviews = (productInfo) => {
         <div class="starsContainer">
         </div>
       </div>
-            <p class="pt-2">${product.description}</p>
+            <p class="pt-2 text-justify">${product.description}</p>
             <p class="text-right">${product.dateTime}</p>
             <hr>
     </div>
@@ -44,6 +80,69 @@ const showReviews = (productInfo) => {
   document.getElementById('reviewContainer').innerHTML = reviewsHtmlContentToAppend;
   showStars(productInfo);
 }
+
+const submitComment = document.getElementById('submitComment');
+const postComments = () => {
+  let txtAreaComments = document.getElementById('txtAreaComments').value;
+  document.getElementById("txtAreaComments").value = " ";
+  let today = new Date();
+  let mes = parseInt(today.getMonth() + 1);
+  mes < 10 ? mes = "0" + mes : '';
+  /*if(mes < 10){
+    mes = '0' + mes
+  }*/
+  today = today.getFullYear() + '-' + mes + '-' + today.getDate() + '  ' + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  console.log(today);
+  if (txtAreaComments) {
+    let htmlCommentToAppend = `
+    <div class="p-4 my-2">
+      <div class="d-flex justify-content-between">
+        <h5 class="font-weight-bold"><i class="fas fa-user mr-1"></i> ${sessionStorage.getItem('user')}</h5>
+        <div class="starsContainer">
+            ${showRating(rating)}
+        </div>
+        </div>
+        <p class="pt-2">${txtAreaComments}</p>
+        <p class="text-right">${today}</p>
+        <hr>
+      </div>
+    </div>
+    `
+
+    document.getElementById("reviewContainer").innerHTML += htmlCommentToAppend;
+  }
+}
+
+const showRelatedProducts = (relatedProductsArray) => {
+  getJSONData(PRODUCTS_URL)
+    .then(resObj => {
+      let relatedProductsHtmlToAppend = '';
+      if (resObj.status === 'ok') {
+        let allProducts = resObj.data;
+        for (let i = 0; i < relatedProductsArray.length; i++) {
+          let relatedProductPosition = relatedProductsArray[i];
+          let related = allProducts[relatedProductPosition];
+
+          relatedProductsHtmlToAppend += `
+          <div class= "col-lg-3 col-md-4 col-6 border m-3">
+            <div id="relatedVideogameImg" class= "row">
+              <img class="img-fluid p-2" src="${related.imgSrc}">                                              
+            </div>                   
+            <div "relatedVideogameInfo" class= "row p-2">
+              <p>${related.name} </p> 
+              <p>${related.description}</p>
+            </div>
+            <div class= "row p-2">
+              <a href="products-info.html">Ver</a>
+            </div>                     
+          </div>
+          `
+        }
+      }
+      document.getElementById("relatedProductContainer").innerHTML = relatedProductsHtmlToAppend;
+    });
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
@@ -68,6 +167,8 @@ document.addEventListener("DOMContentLoaded", function (e) {
       productCategoryHTML.innerHTML = product.category;
 
       showImagesGallery(product.images);
+
+      showRelatedProducts(product.relatedProducts);
     }
   });
   getJSONData(PRODUCT_INFO_COMMENTS_URL).then(resultObj => {
@@ -76,5 +177,5 @@ document.addEventListener("DOMContentLoaded", function (e) {
     showReviews(productComments);
   });
 
-
+  submitComment.addEventListener("click", postComments);
 });
