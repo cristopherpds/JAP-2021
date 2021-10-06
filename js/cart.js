0ad21a1004e4c0cda22a777054017f99308d57b9
Cristopher <josuepaiva7@gmail.com>
2021-10-05 21:23:32 -0300

    The web request was made where a cart with preloaded products in found, and it is shown in HTML with its information and its graphics elements.

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,57 @@
+//Esta funcin muestra los productos del carrito
+const showCart = (array) => {
+  let htmlToAppend = '';
+  for (let i = 0; i < array.articles.length; i++) {
+    let product = array.articles[i];
+    htmlToAppend += `
+    <tr>
+      <td><img style= width:100px; src="${product.src}" alt=""</td>
+      <td>${product.name}</td>
+      <td>${product.currency} ${array.articles[i].unitCost}</td>
+        <td>
+          <input class="form-control" style= width:90px; type="number" id="productCount"  value=${product.count} min="1"/>
+        </td>
+      <td><span id"productSubtotal">${productSubTotal(product)}</span></td>
+      </tr>
+    `
+
+    document.getElementById("cart-products").innerHTML = htmlToAppend;
+
+    updateSubtotal(array)
+
+    document.getElementById("productCount").addEventListener('change', () => {
+      updateSubtotal(array);
+      productSubTotal(product)
+    });
+  }
+}
+
+const updateSubtotal = (array) => {
+  for (let i = 0; i < array.articles.length; i++) {
+    let product = array.articles[i];
+    let productUnitCost = product.unitCost;
+    let productCurrency = product.currency;
+
+    let count = document.getElementById('productCount').value;
+    let subtotal = count * productUnitCost;
+
+    document.getElementById('subtotal').innerHTML = `${productCurrency}&nbsp &nbsp${subtotal}`;
+    document.getElementById('total').innerHTML = `${productCurrency}&nbsp &nbsp${subtotal}`;
+  }
+}
+
+function productSubTotal(product){
+  return product.unitCost * product.count;
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-
+document.addEventListener("DOMContentLoaded", function (e) {
+  getJSONData(CART_INFO_URL2).then(resObj => {
+    if (resObj.status === 'ok') {
+      let array = resObj.data;
+      showCart(array)
+    }
+  });
 });
--- js/init.js
+++ js/init.js
@@ -5,6 +5,7 @@ const PRODUCTS_URL = "https://japdevdep.github.io/ecommerce-api/product/all.json
 const PRODUCT_INFO_URL = "https://japdevdep.github.io/ecommerce-api/product/5678.json";
 const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/product/5678-comments.json";
 const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
+const CART_INFO_URL2 = "https://japdevdep.github.io/ecommerce-api/cart/654.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 
 var showSpinner = function () {
