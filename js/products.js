627298ce2e8066c969a687c4e232fa6716a72bf9
Cristopher <josuepaiva7@gmail.com>
2021-11-01 00:10:59 -0300

    refactoring the product part, card creation, and display in grid mode

--- js/products.js
+++ js/products.js
@@ -40,7 +40,32 @@ const showProductsList = () => {
         if (((minCount == undefined) || (minCount != undefined && parseInt(products.cost) >= minCount)) &&
             ((maxCount == undefined) || (maxCount != undefined && parseInt(products.cost) <= maxCount))) {
 
-            htmlContentToAppend += `
+            htmlContentToAppend +=
+                `
+                <div class="col-md-4 col-sm-4 col-lg-4">
+                    <div class="card mb-4 shadow-sm">
+                        <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
+                        <div class="card-body">
+                            <h4 class="mb-1">`+ products.name + `</h4>
+                            <p>`+ products.description + `</p>
+                            <p> A tan solo `+ products.currency + "   " + products.cost + `</p>
+                            <p class="card-text"></p>
+                        <div class="card-footer text-muted">
+                            ${products.soldCount} artículos vendidos.
+                        </div>
+                        <br>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-sm btn-outline-secondary" onclick="window.location.href='/product-info.html'" >Ver</button>
+                            </div>
+                                <small class="text-muted">9 mins</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+        `
+            /*`
             <a href="product-info.html" class="product-list-group-item list-group-item-action">
                 <div class="list-group-item list-group-item-action">
                     <div class="row">
@@ -60,7 +85,7 @@ const showProductsList = () => {
                     </div>
                 </div>
             </a>
-            `
+            `*/
         }
         document.getElementById("product-list-container").innerHTML = htmlContentToAppend;
     }
